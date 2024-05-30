caee56097902abb6566600371aa819c92bc308f6
Asik8 <motasembillah84@gmail.com>
2024-05-30 15:00:35 +0600

    f little modification done

--- F_Equation.cpp
+++ F_Equation.cpp
@@ -6,7 +6,7 @@ int main() {
     int x,n;
     cin>>x>>n;
     ll sum = 0;
-    for (int i=2;i<n;i+=2) sum+=pow(x,i);
+    for (int i=2;i<=n;i+=2) sum+=pow(x,i);
     cout<<sum<<endl;
     return 0;
 }
